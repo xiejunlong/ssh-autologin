de7947320989c79d0843cf23fff3b3dff9f0078e
xiejunlong <x_5656@sina.com>
2021-08-24 13:43:42 +0800

    Update goto.ex
    添加sudo_root、执行自定义命令

--- goto.ex
+++ goto.ex
@@ -5,6 +5,8 @@ set IP [lindex $argv 1]
 set PORT [lindex $argv 2]
 set USER_NAME [lindex $argv 3]
 set PASSWORD [lindex $argv 4]
+set SUDO_ROOT [lindex $argv 5]
+set COMMAND [lindex $argv 6]
 
 spawn ssh -p $PORT $USER_NAME@$IP
 
@@ -13,11 +15,31 @@ expect {
     "*assword" { send "$PASSWORD\r\n"; exp_continue ; sleep 3; }
     "yes/no" { send \"yes\n\"; exp_continue; }
     "Last*" {
-        puts "\n登录成功\n";
-        send "PROMPT_COMMAND='echo -ne \"\\033]0;$SERVER_NAME \\007\"' \r";
-        send "clear\r";
+        puts "\n$USER_NAME登录$SERVER_NAME成功\n";
+#        send "PROMPT_COMMAND='echo -ne \"\\033]0;$SERVER_NAME \\007\"' \r";
+        send -- "clear\r";
     }
     timeout { puts "Expect was timeout."; return }
 }
+
+if { "$SUDO_ROOT" == 1 } {
+    puts "$USER_NAME 正在 sudo -i\n"
+    expect "${USER_NAME}@"
+    send "sudo -i\n"
+    expect "${USER_NAME}:"
+    send "$PASSWORD\r\n"
+    expect "~]#"
+    send -- "clear\r";
+}
+
+if { "$COMMAND" != "" } {
+    send "$COMMAND\r"
+}
+
+# 登录后要运行的其他命令
+#send -- "alias ls='ls -F --color'\n"
+#send -- "alias ll='ls -l'\n"
+#send -- "alias grep='grep --color'\n"
+#send -- "alias egrep='egrep --color'\n"
 
 interact
