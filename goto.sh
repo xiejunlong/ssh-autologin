a961d5fe9108eb26231a44565bbbadc33fe7d32e
xiejunlong <x_5656@sina.com>
2021-08-24 13:56:11 +0800

    Update goto.sh
    添加sudo_root和执行自定义命令

--- goto.sh
+++ goto.sh
@@ -10,8 +10,9 @@
 # 		$ echo "alias goto=\"$PWD/goto.sh\"" >> ~/.zshrc
 # 		$ source ~/.zshrc
 # 		$ goto
-# Author: whoru.S.Q <whoru@sqiang.net>
-# Version: 1.5.1
+# Author: junlong xie
+# # ReferenceLink：https://github.com/whorusq/ssh-autologin
+# Version: 1
 ################################################
 
 # 服务器列表文件
@@ -23,7 +24,7 @@ CONFIG_ARR=()
 
 # 记录默认分隔符，用于修改后的还原
 IFS_OLD=$IFS
-
+	
 # 初始化
 function menu {
 
@@ -106,27 +107,17 @@ function sshLogin {
 		pwd="123456"
 	fi
 
+    	# 默认切换到root
+    	local sudo_root=${config[5]}
+    	if [[ $sudo_root == "" ]]; then
+        	sudo_root="1"
+    	fi
+
 	# 开始登录
-	echo -e "\n\n==> 正在登录【${config[0]}】，请稍等...\n"
+	echo -e "\n\n\033[32m==>\033[0m 正在登录【\033[32m${config[0]}\033[0m】，请稍等...\n"
 	sleep 1
-	expect -c "
-	    spawn ssh $user@${config[2]} -p $port
-	    expect {
-	        \"*assword\" {set timeout 6000; send \"$pwd\n\"; exp_continue ; sleep 3; }
-	        \"yes/no\" {send \"yes\n\"; exp_continue;}
-	        \"Last*\" {  send_user \"\n已经成功登录【${config[0]}】\n\";}
-	    }
-	    if {\"${config[5]}\" != \"\"} {
-			expect \"*]#\"
-			send \"${config[5]}\r\"
-		}
-		interact"
-	echo -e "\n==> 您已退出【${config[0]}】\n"
-	
-	#echo -e "\n\n\033[32m==>\033[0m 正在登录【\033[32m${config[0]}\033[0m】，请稍等...\n"
-	#sleep 1
-	#$(which expect) $BASE_PATH/goto.ex ${config[0]} ${config[2]} $port $user $pwd
-	#echo -e "\n\033[32m==>\033[0m 您已退出【\033[32m${config[0]}\033[0m】\n"
+	$(which expect) $BASE_PATH/goto.ex ${config[0]} ${config[2]} $port $user $pwd $sudo_root ${config[6]}
+	echo -e "\n\033[32m==>\033[0m 您已退出【\033[32m${config[0]}\033[0m】\n"
 }
 
 # 执行初始化
